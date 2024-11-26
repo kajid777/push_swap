630b2300f94d6bd1d23597bf6af824ed8ab7bd62
Daichi Kajiwara <dkajiwar@c1r6s2.42tokyo.jp>
2024-11-26 22:03:42 +0900

    fixed a little.

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: dkajiwar <dkajiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 22:47:34 by dkajiwar          #+#    #+#             */
-/*   Updated: 2024/11/26 20:28:16 by dkajiwar         ###   ########.fr       */
+/*   Updated: 2024/11/26 22:01:43 by dkajiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,12 @@ void sort(Node **ptr_topoflist_a, Node **ptr_topoflist_b)
 	Node *head_a;
 	
 	i = 0;
-	
 	init_stack_a_size = get_stack_size(*ptr_topoflist_a);
 	max_bit_len = get_max_bit_len(*ptr_topoflist_a);
-	(void)ptr_topoflist_b;
 	ft_printf("test:max_bit_len:%d\n", max_bit_len);
 	ft_printf("test:stack_a_size:%d\n", init_stack_a_size);
-	
 	while(i < max_bit_len)
 	{
-		
 		j = 0;
 		k = 0;
 		while(j < init_stack_a_size)
